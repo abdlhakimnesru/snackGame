499a401bbe968f90fd00856ff1bf5c5bf6d83030
Abdulhakim <abdlhakimnesrugmail.com>
2026-01-17 17:12:45 +0300

    unshift add

--- JS/script.js
+++ JS/script.js
@@ -1,36 +1,97 @@
-const canvas= document.querySelector(".canvas");
+// const canvas= document.querySelector(".canvas");
+// const ctx = canvas.getContext("2d");
+
+// console.log(ctx)
+
+// const scale = 20;
+// const  rows = canvas.height/scale;
+// const columns = canvas.width/scale;
+
+// let snake = [];
+// snake[0] = {
+//     x:Math.floor(Math.random()*columns)*scale, 
+//     y:Math.floor(Math.random()*rows)*scale
+// };
+
+// let d = "right";
+
+// let playgame = setInterval(draw, 100);
+
+// function draw()
+// {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     for(let i=0; i<snake.length; i++){
+
+//         ctx.fillStyle = "#fff";
+//         ctx.strokeStyle = "red";
+
+//         ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
+//         ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
+//         }
+
+
+//         let snakeX = snake[0].x;
+//         let snakeY = snake[0].y;
+//         console.log(snakeX)
+
+
+//         if(d === "RIGHT") snakeX += scale;
+//         if(d === "LEFT") snakeX -= scale;
+//         if(d === "UP") snakeY -= scale;
+//         if(d === "DOWN") snakeY += scale;
+//         let newHead = {
+//             x: snakeX,
+//             y: snakeY
+//         }
+
+//         snake.unshift(newHead);
+//     }
+
+
+const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
 console.log(ctx)
 
 const scale = 20;
-const  rows = canvas.height/scale;
-const columns = canvas.width/scale;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
 
 let snake = [];
 snake[0] = {
-    x:Math.floor(Math.random()*columns)*scale, 
-    y:Math.floor(Math.random()*rows)*scale
+  x: Math.floor(Math.random() * columns) * scale,
+  y: Math.floor(Math.random() * rows) * scale
 };
 
+// FIX: use uppercase to match conditions
+let d = "RIGHT";
 
-let playgame = setInterval(draw, 100);
+setInterval(draw, 100);
 
-function draw(){
-ctx.fillStyle = "#fff";
-ctx.fillRect(snake[0].x, snake[0].y, scale, scale);
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-ctx.strokeRect(snake[0].x, snake[0].y, scale, scale);
-ctx.strokeStyle = "red";
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = "#fff";
+    ctx.strokeStyle = "red";
+    ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
+    ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
+  }
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  console.log(snakeX);
+
+  if (d === "RIGHT") snakeX += scale;
+  if (d === "LEFT") snakeX -= scale;
+  if (d === "UP") snakeY -= scale;
+  if (d === "DOWN") snakeY += scale;
+
+  let newHead = { x: snakeX, y: snakeY };
+
+  snake.unshift(newHead);
+
+  // prevent infinite growth (core snake move)
+//   snake.pop();
 }
-
-let snakeX = snake[0].x;
-let snakeY = snake[0].y;
-
-
-if(d="RIGHT") snakeX += scale;
-if(d="LEFT") snakeX -= scale;
-if(d="UP") snakeY -= scale;
-if(d="DOWN") snakeY += scale;
-
-
