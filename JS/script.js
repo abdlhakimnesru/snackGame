8ff776369a4f5ce36d4660a7c4536a924086ca66
Abdulhakim <abdlhakimnesrugmail.com>
2026-01-20 10:43:56 +0300

    unshift

--- JS/script.js
+++ JS/script.js
@@ -1,52 +1,3 @@
-// const canvas= document.querySelector(".canvas");
-// const ctx = canvas.getContext("2d");
-
-// console.log(ctx)
-
-// const scale = 20;
-// const  rows = canvas.height/scale;
-// const columns = canvas.width/scale;
-
-// let snake = [];
-// snake[0] = {
-//     x:Math.floor(Math.random()*columns)*scale, 
-//     y:Math.floor(Math.random()*rows)*scale
-// };
-
-// let d = "right";
-
-// let playgame = setInterval(draw, 100);
-
-// function draw()
-// {
-//     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//     for(let i=0; i<snake.length; i++){
-
-//         ctx.fillStyle = "#fff";
-//         ctx.strokeStyle = "red";
-
-//         ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
-//         ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
-//         }
-
-
-//         let snakeX = snake[0].x;
-//         let snakeY = snake[0].y;
-//         console.log(snakeX)
-
-
-//         if(d === "RIGHT") snakeX += scale;
-//         if(d === "LEFT") snakeX -= scale;
-//         if(d === "UP") snakeY -= scale;
-//         if(d === "DOWN") snakeY += scale;
-//         let newHead = {
-//             x: snakeX,
-//             y: snakeY
-//         }
-
-//         snake.unshift(newHead);
-//     }
 
 
 const canvas = document.querySelector(".canvas");
@@ -64,34 +15,87 @@ snake[0] = {
   y: Math.floor(Math.random() * rows) * scale
 };
 
+ let food = {
+        x: Math.floor(Math.random() * columns) * scale,
+        y: Math.floor(Math.random() * rows) * scale
+      };
+
 // FIX: use uppercase to match conditions
-let d = "RIGHT";
+let d = "LEFT";
+
+document.onkeydown = direction;
+
+function direction(event) {
+  if (event.keyCode === 37 && d !== "RIGHT") d = "LEFT";
+  else if (event.keyCode === 38 && d !== "DOWN") d = "UP";
+  else if (event.keyCode === 39 && d !== "LEFT") d = "RIGHT";
+  else if (event.keyCode === 40 && d !== "UP") d = "DOWN";
+}
 
 setInterval(draw, 100);
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let i = 0; i < snake.length; i++) {
-    ctx.fillStyle = "#fff";
-    ctx.strokeStyle = "red";
-    ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
-    ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
-  }
+      for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = "#fff";
+        ctx.strokeStyle = "red";
+        ctx.fillRect(snake[i].x, snake[i].y, scale, scale);
+        ctx.strokeRect(snake[i].x, snake[i].y, scale, scale);
+      }
 
-  let snakeX = snake[0].x;
-  let snakeY = snake[0].y;
-  console.log(snakeX);
+      // draw food
+     
 
-  if (d === "RIGHT") snakeX += scale;
-  if (d === "LEFT") snakeX -= scale;
-  if (d === "UP") snakeY -= scale;
-  if (d === "DOWN") snakeY += scale;
+      ctx.fillStyle = "#ff0";
+      ctx.strokeStyle = "green";
+      
+      ctx.fillRect(food.x, food.y, scale, scale);
+      ctx.strokeRect(food.x, food.y, scale, scale);
 
-  let newHead = { x: snakeX, y: snakeY };
+      let snakeX = snake[0].x;
+      let snakeY = snake[0].y;
+      console.log(snakeX);
 
-  snake.unshift(newHead);
+      
 
-  // prevent infinite growth (core snake move)
-//   snake.pop();
-}
+
+      if (d === "RIGHT") snakeX += scale;
+      if (d === "LEFT") snakeX -= scale;
+      if (d === "UP") snakeY -= scale;
+      if (d === "DOWN") snakeY += scale;
+
+      if (snakeX >= canvas.width) snakeX = 0;
+      if (snakeX < 0) snakeX = canvas.width - scale;
+
+      if (snakeY >= canvas.height) snakeY = 0;
+      if (snakeY < 0) snakeY = canvas.height - scale;
+
+
+      if (snakeX === food.x && snakeY === food.y) {
+        food = {
+          x: Math.floor(Math.random() * columns) * scale,
+          y: Math.floor(Math.random() * rows) * scale
+        };
+      } else {
+        snake.pop();
+      }
+
+
+
+      let newHead = { x: snakeX, y: snakeY };
+
+      snake.unshift(newHead);
+    }
+
+      // checkCollision function can be added here for self-collision detection
+
+      function checkCollision(head, array)
+       {
+            for (let i = 0; i < array.length; i++) {
+              if (head.x === array[i].x && head.y === array[i].y) {
+                return true;
+              }
+            }
+            return false;
+      }
